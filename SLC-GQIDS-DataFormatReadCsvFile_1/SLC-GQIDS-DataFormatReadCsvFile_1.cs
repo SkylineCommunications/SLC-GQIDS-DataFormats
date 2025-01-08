49894e9a4774e4819092359908c67a7e056d5be0
SKYLINE2\CarlosMT <carlos.matheus@skyline.be>
2025-01-08 15:43:26 -0500

    Add project files.

---
+++ SLC-GQIDS-DataFormatReadCsvFile_1/SLC-GQIDS-DataFormatReadCsvFile_1.cs
@@ -0,0 +1,387 @@
+namespace SLCGQIDSDataFormatReadCsvFile_1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+
+    using CsvHelper;
+    using CsvHelper.Configuration;
+    using CsvHelper.TypeConversion;
+
+    using Skyline.DataMiner.Analytics.GenericInterface;
+    using Skyline.DataMiner.Net.Messages.SLDataGateway;
+    using Skyline.DataMiner.Utils.SecureCoding.SecureIO;
+
+    [GQIMetaData(Name = "CSV File")]
+    public sealed class SLCGQIDSDataFormatReadCsvFile : IGQIDataSource, IGQIInputArguments, IGQIOnInit, IGQIUpdateable
+    {
+        private const string CSV_ROOT_PATH = @"C:\Skyline DataMiner\Documents\DataMiner Catalog\DevOps\Ad Hoc Data Sources\SLC-GQIDS-DataFormatReadCsvFile";
+
+        private readonly string[] commonDelimeters = { ",", ";", "\t", "|" };
+        private readonly GQIStringArgument fileName = new GQIStringArgument("File name") { IsRequired = true };
+        private readonly GQIStringArgument delimiter = new GQIStringArgument("Delimiter") { IsRequired = false };
+        private readonly GQIStringDropdownArgument headerCapitalization = new GQIStringDropdownArgument("Header capitalization", new[] { "Original", "Uppercase", "Titlecase", "Lowercase" }) { IsRequired = false, DefaultValue = "Original" };
+
+        private readonly DateTimeConverter _dateTimeConverter = new DateTimeConverter();
+
+        private GQIDMS _dms;
+
+        private IGQILogger _logger;
+        private string _fileName;
+        private string _delimiter;
+        private string _headerCapitalization;
+        private string _csvFilePath;
+
+
+        private HeaderInfo _headerInfo;
+        private GQIRow[] _rows;
+        private int _rowCount;
+        private IGQIUpdater _updater;
+        private FileSystemWatcher _watcher;
+
+        public OnInitOutputArgs OnInit(OnInitInputArgs args)
+        {
+            _dms = args.DMS;
+            _logger = args.Logger;
+            return new OnInitOutputArgs();
+        }
+
+        public GQIArgument[] GetInputArguments()
+        {
+            return new GQIArgument[] { fileName, delimiter, headerCapitalization };
+        }
+
+        public OnArgumentsProcessedOutputArgs OnArgumentsProcessed(OnArgumentsProcessedInputArgs args)
+        {
+            _headerInfo = default;
+            _rows = null;
+
+            var securePath = SecurePath.CreateSecurePath(CSV_ROOT_PATH);
+            if (!Directory.Exists(securePath))
+            {
+                Directory.CreateDirectory(securePath);
+            }
+
+            _fileName = args.GetArgumentValue(fileName);
+
+            if (!_fileName.Contains(".csv"))
+            {
+                _fileName = $"{_fileName}.csv";
+            }
+
+            _csvFilePath = SecurePath.ConstructSecurePath(CSV_ROOT_PATH, _fileName);
+
+            if (!File.Exists(_csvFilePath))
+                throw new GenIfException($"Csv file does not exist: {_csvFilePath}");
+
+            _delimiter = args.GetArgumentValue(delimiter);
+            _headerCapitalization = args.GetArgumentValue(headerCapitalization);
+
+            ReadCSVFile();
+
+            return new OnArgumentsProcessedOutputArgs();
+        }
+
+        private void ReadCSVFile()
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = DetectDelimiter(_csvFilePath),
+            };
+
+            using (var fileStream = new FileStream(_csvFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
+            using (var streamReader = new StreamReader(fileStream))
+            using (var csvReader = new CsvReader(streamReader, config))
+            {
+                csvReader.Read();
+                csvReader.ReadHeader();
+                _headerInfo = GetHeaderInfo(csvReader.HeaderRecord);
+                _rows = ReadRows(csvReader);
+                _rowCount = _rows.Length;
+            }
+        }
+
+        private string DetectDelimiter(string filePath)
+        {
+            if (!String.IsNullOrEmpty(_delimiter))
+            {
+                return _delimiter;
+            }
+
+            Dictionary<string, int> delimiterCounts = new Dictionary<string, int>();
+
+            using (var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
+            using (var streamReader = new StreamReader(fileStream))
+            {
+                string headerLine = streamReader.ReadLine();
+
+                foreach (var delimiter in commonDelimeters)
+                {
+                    delimiterCounts[delimiter] = headerLine.Split(new[] { delimiter }, StringSplitOptions.None).Length - 1;
+                }
+
+                var detectedDelimiter = delimiterCounts.OrderByDescending(pair => pair.Value).FirstOrDefault();
+
+                if (detectedDelimiter.Value > 0)
+                {
+                    return detectedDelimiter.Key;
+                }
+
+                throw new GenIfException($"No Delimiter detected, please specify a delimiter.");
+            }
+        }
+
+        private GQIColumn GetColumn(string name, string type)
+        {
+            var headerCapitalizedName = GetHeaderCapitalization(name);
+
+            switch (type)
+            {
+                case "bool": return new GQIBooleanColumn(headerCapitalizedName);
+                case "datetime": return new GQIDateTimeColumn(headerCapitalizedName);
+                case "double": return new GQIDoubleColumn(headerCapitalizedName);
+                case "int": return new GQIIntColumn(headerCapitalizedName);
+                default: return new GQIStringColumn(headerCapitalizedName);
+            }
+        }
+
+        private string GetHeaderCapitalization(string headerName)
+        {
+            switch (_headerCapitalization)
+            {
+                case "Uppercase":
+                    return headerName.ToUpper();
+
+                case "Lowercase":
+                    return headerName.ToLower();
+
+                case "Titlecase":
+                    TextInfo textInfo = CultureInfo.InvariantCulture.TextInfo;
+                    return textInfo.ToTitleCase(headerName.ToLower());
+
+                case "Original":
+                default:
+                    return headerName;
+            }
+        }
+
+        private HeaderInfo GetHeaderInfo(string[] header)
+        {
+            var keyIndex = -1;
+            var columns = new List<GQIColumn>();
+
+            for (int i = 0; i < header.Length; i++)
+            {
+                var columnInfo = GetColumnInfo(header[i]);
+
+                if (columnInfo.type == "key")
+                {
+                    if (keyIndex != -1)
+                        throw new GenIfException($"Duplicate key definition at column {keyIndex} and column {i}.");
+                    keyIndex = i;
+                }
+
+                var column = GetColumn(columnInfo.name, columnInfo.type);
+                columns.Add(column);
+            }
+
+            return new HeaderInfo(keyIndex, columns.ToArray());
+        }
+
+        private (string name, string type) GetColumnInfo(string head)
+        {
+            var separatorIndex = head.IndexOf("::");
+            if (separatorIndex == -1)
+                return (head, "string");
+
+            var name = head.Substring(0, separatorIndex);
+            var type = head.Substring(separatorIndex + 2);
+            return (name, type);
+        }
+
+        private GQIRow[] ReadRows(CsvReader csvReader)
+        {
+            var columnTypes = _headerInfo.GetColumnTypes();
+            return ReadRows(csvReader, columnTypes);
+        }
+
+        private GQIRow[] ReadRows(CsvReader reader, Type[] columnTypes)
+        {
+            var rows = new List<GQIRow>();
+            while (reader.Read())
+            {
+                var row = ReadRow(reader, columnTypes, rows.Count);
+                rows.Add(row);
+            }
+
+            return rows.ToArray();
+        }
+
+        private GQIRow ReadRow(CsvReader reader, Type[] columnTypes, int key)
+        {
+            var cells = columnTypes.Select((type, index) => GetCell(reader, index, type));
+            return new GQIRow(key.ToString(), cells.ToArray());
+        }
+
+        private GQIRow ReadRow(CsvReader reader, int keyIndex, Type[] columnTypes)
+        {
+            var key = reader.GetField(keyIndex);
+            var cells = columnTypes.Select((type, index) => GetCell(reader, index, type));
+            return new GQIRow(key, cells.ToArray());
+        }
+
+        private GQICell GetCell(CsvReader reader, int index, Type type)
+        {
+            if (type == typeof(DateTime))
+            {
+                var dateTime = reader.GetField<DateTime>(index, _dateTimeConverter);
+                return new GQICell() { Value = dateTime };
+            }
+
+            var value = reader.GetField(type, index);
+            return new GQICell() { Value = value };
+        }
+
+        public GQIColumn[] GetColumns()
+        {
+            return _headerInfo.Columns;
+        }
+
+        public GQIPage GetNextPage(GetNextPageInputArgs args)
+        {
+            return new GQIPage(_rows);
+        }
+
+        private void UpdateRows()
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = DetectDelimiter(_csvFilePath),
+            };
+
+            using (var fileStream = new FileStream(_csvFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
+            using (var streamReader = new StreamReader(fileStream))
+            using (var csvReader = new CsvReader(streamReader, config))
+            {
+                csvReader.Read(); // Skip header
+                var columnTypes = _headerInfo.GetColumnTypes();
+                UpdateRows(csvReader, columnTypes);
+            }
+        }
+
+        private void UpdateRows(CsvReader reader, Type[] columnTypes)
+        {
+            int index = 0;
+
+            RemoveExtraRows(index);
+            // Process new or updated rows
+            while (reader.Read())
+            {
+                var row = ReadRow(reader, columnTypes, index);
+                _updater.AddRow(row);
+
+                index++;
+            }
+
+            // Process deleted rows
+        }
+
+        private void RemoveExtraRows(int startingIndex)
+        {
+            for (int i = 0; i < _rowCount; i++)
+            {
+                _updater.RemoveRow($"{i}");
+            }
+        }
+
+        public void OnStartUpdates(IGQIUpdater updater)
+        {
+            _updater = updater;
+
+            var directory = Path.GetDirectoryName(_csvFilePath);
+            var fileName = Path.GetFileName(_csvFilePath);
+            _watcher = new FileSystemWatcher(directory, fileName) { NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.CreationTime, EnableRaisingEvents = true };
+
+            _watcher.Changed += OnChanged;
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs args)
+        {
+            try
+            {
+                _logger.Information("OnChange detected");
+                _logger.Debug("OnChange detected");
+                UpdateRows();
+            }
+            catch (Exception ex)
+            {
+                _logger.Information($"Failed to update rows: {ex.Message}");
+                throw new GenIfException($"Failed to update rows: {ex.Message}");
+            }
+        }
+
+        public void OnStopUpdates()
+        {
+            if (_watcher is null)
+                return;
+
+            _watcher.Changed -= OnChanged;
+            _watcher.Dispose();
+            _updater = null;
+        }
+
+
+        private class HeaderInfo
+        {
+            public int KeyIndex { get; }
+
+            public GQIColumn[] Columns { get; }
+
+            public HeaderInfo(int keyIndex, GQIColumn[] columns)
+            {
+                KeyIndex = keyIndex;
+                Columns = columns;
+            }
+
+            public Type[] GetColumnTypes()
+            {
+                return Columns.Select(column => GetColumnType(column.Type)).ToArray();
+            }
+
+            private Type GetColumnType(GQIColumnType type)
+            {
+                switch (type)
+                {
+                    case GQIColumnType.Boolean: return typeof(bool);
+                    case GQIColumnType.DateTime: return typeof(DateTime);
+                    case GQIColumnType.Double: return typeof(double);
+                    case GQIColumnType.Int: return typeof(int);
+                    default: return typeof(string);
+                }
+            }
+        }
+
+        private class DateTimeConverter : ITypeConverter
+        {
+            public object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+            {
+                try
+                {
+                    return DateTime.SpecifyKind(DateTime.Parse(text), DateTimeKind.Utc);
+                }
+                catch (FormatException)
+                {
+                    throw new GenIfException(text);
+                }
+            }
+
+            public string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+            {
+                return value.ToString();
+            }
+        }
+    }
+}
